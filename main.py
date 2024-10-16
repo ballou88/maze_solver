cc67b9ef346f6937239bb6d57cacd78975bc2713
Mike Ballou <mikeballou@gmail.com>
2024-10-16 16:04:36 -0700

    Updated defualt size

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from maze import Maze
 def main():
     win = Window(800, 600)
 
-    maze = Maze(10, 10, 10, 5, 10, 10, win)
+    maze = Maze(10, 10, 15, 15, 30, 30, win)
     maze.solve()
     win.wait_for_close()
 
